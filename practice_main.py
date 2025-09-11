40c3ed83f0b9ef0f00fa41d0fa5ddb351ed16fa7
Charles-noob <charleshsu.11@gmail.com>
2025-09-11 20:56:13 +0800

    fuck u python

--- practice_main.py
+++ practice_main.py
@@ -9,12 +9,14 @@ bg=pg.Surface(screen.get_size())
 bg=bg.convert()
 bg.fill((255,255,255)) # white
 
-pg.draw.rect(bg,(0,0,255),(50,50,100,100),2) #blue rectangle
-pg.draw.circle(bg,(255,0,0),(200,200),50) #red circle
-pg.draw.line(bg,(0,255,0),(300,300),(400,400),5) #green line
+#pg.draw.rect(bg,(0,0,255),(50,50,100,100),2) #blue rectangle
+#pg.draw.circle(bg,(255,0,0),(200,200),50) #red circle
+#pg.draw.line(bg,(0,255,0),(300,300),(400,400),5) #green line
+title=pg.font.SysFont("arial",72)
+titletext=title.render("FUCK U PYTHON",True,(0,0,0)) #black text
 screen.blit(bg,(0,0))#display the background
+screen.blit(titletext,(100,100))
 pg.display.update()
-
 # main loop
 running=True
 while running:
