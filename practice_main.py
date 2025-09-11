faa3b32c1475b68e80f2c5cc34b42403cfe373f6
Charles-noob <charleshsu.11@gmail.com>
2025-09-11 19:59:28 +0800

    display the screen

--- practice_main.py
+++ practice_main.py
@@ -1,3 +1,20 @@
 import pygame as pg
 
 pg.init()
+w,h=400,400
+screen=pg.display.set_mode((w,h))
+pg.display.set_caption("practice")
+
+bg=pg.Surface(screen.get_size())
+bg=bg.convert()
+bg.fill((255,255,255)) # white
+screen.blit(bg,(0,0))#display the background
+pg.display.update()
+
+# main loop
+running=True
+while running:
+    for event in pg.event.get():
+        if event.type==pg.QUIT:
+            running=False
+pg.quit()
