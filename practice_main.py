890e6387462ac80ff581cf04d9ac998ddd9ada51
Charles-noob <charleshsu.11@gmail.com>
2025-09-11 20:04:37 +0800

    draw something

--- practice_main.py
+++ practice_main.py
@@ -1,13 +1,17 @@
 import pygame as pg
 
 pg.init()
-w,h=400,400
+w,h=1300,700
 screen=pg.display.set_mode((w,h))
 pg.display.set_caption("practice")
 
 bg=pg.Surface(screen.get_size())
 bg=bg.convert()
 bg.fill((255,255,255)) # white
+
+pg.draw.rect(bg,(0,0,255),(50,50,100,100),2) #blue rectangle
+pg.draw.circle(bg,(255,0,0),(200,200),50) #red circle
+pg.draw.line(bg,(0,255,0),(300,300),(400,400),5) #green line
 screen.blit(bg,(0,0))#display the background
 pg.display.update()
 
