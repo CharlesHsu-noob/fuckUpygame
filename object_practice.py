4043f7f2b956146df8efeccccaa0d4d50373af8e
Charles-noob <charleshsu.11@gmail.com>
2025-09-12 22:32:54 +0800

    2

--- object_practice.py
+++ object_practice.py
@@ -3,16 +3,23 @@ import random,math
 pg.init()
 pg.mixer.init()
 clock=pg.time.Clock()
-
-objectlist=pg.sprite.Group()
 screeninfo=pg.display.Info()
 w,h=screeninfo.current_w,screeninfo.current_h-80
 screen = pg.display.set_mode((w,h))
 pg.display.set_caption("object_practice")
-
 bg=pg.Surface(screen.get_size())
 bg=bg.convert()
 bg.fill((255,255,255)) # white
+
+mrbeast=pg.image.load("picture/mrbeast.png")
+class mrbeast:
+    def __init()__:
+        self.image=pg.transform.scale(mrbeast,(200,130))
+        self.rect=self.image.get_rect()
+        self.rect.center=(300,550)
+        self.v=7
+    def move()
+objectlist=pg.sprite.Group()
 
 #main loop
 running=True
