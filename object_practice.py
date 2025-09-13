f1bc01ca571bc292b70167e16beefc2ddea43928
Charles-noob <charleshsu.11@gmail.com>
2025-09-13 20:28:18 +0800

    main menu 2

--- object_practice.py
+++ object_practice.py
@@ -10,8 +10,10 @@ pg.display.set_caption("object_practice")
 bg=pg.Surface(screen.get_size())
 bg=bg.convert()
 bg.fill((255,255,255)) # white
-mainMenuBg=pg.transform.scale(pg.image.load("picture/main_menu_bg.png").convert_alpha(),(w,h))
-objectlist=pg.sprite.Group()
+main_menu_bg_or=pg.image.load("picture/main_menu_bg.png")
+main_menu_bg_or.convert()
+mainMenuBg=pg.transform.scale(main_menu_bg_or.convert_alpha(),(w,h))
+main_menu_objectlist=pg.sprite.Group()
 
 class moveObject(pg.sprite.Sprite):
     def __init__(self,picture_path,center,size,v,israndom):
@@ -21,6 +23,7 @@ class moveObject(pg.sprite.Sprite):
         self.rect=self.image.get_rect(center=center)
         if israndom:
             self.pos=(random.randint(20,70))
+            self.pos=math.radians(self.pos)
         else:
             self.pos=0
         self.dx=v*math.cos(self.pos)
@@ -34,26 +37,65 @@ class moveObject(pg.sprite.Sprite):
         if(self.rect.top<=0 or self.rect.bottom>=screen.get_height()):
             self.dy*=-1
 
+class buttonObject(pg.sprite.Sprite):
+    def __init__(self,picture_paths,center,size):
+        super().__init__()
+        self.istouch=False
+        self.ispress=False
+        self.images = [
+            pg.transform.scale(pg.image.load(picture_paths[0]).convert_alpha(), size),
+            pg.transform.scale(pg.image.load(picture_paths[1]).convert_alpha(), size),
+            pg.transform.scale(pg.image.load(picture_paths[2]).convert_alpha(), size)
+        ]
+        self.image=self.images[0]
+        self.rect=self.image.get_rect(center=center)
+    def update(self):
+        mouse_pos = pg.mouse.get_pos()
+        mouse_pressed = pg.mouse.get_pressed()[0]
+        # 使用 collidepoint 進行更簡單的碰撞偵測
+        if self.rect.collidepoint(mouse_pos):
+            self.image = self.images[1]
+            self.istouch=True
+        else:
+            self.image = self.images[0]
+            self.istouch=False
+        if self.istouch and mouse_pressed:
+            self.image=self.images[2]
+            self.ispress=True
+        elif self.istouch:
+            self.image=self.images[1]
+            self.ispress=False
+        else:
+            self.image=self.images[0]
+            self.ispress=False
+
 
 mrbeast=moveObject("picture/MrBeast.png",(300,550),(200,130),7,False)
-objectlist.add(mrbeast)
+main_menu_objectlist.add(mrbeast)
 milk=moveObject("picture/milkdragon.png",(random.randint(100,250),random.randint(150,250)),(130,170),8,True)
-objectlist.add(milk)
+main_menu_objectlist.add(milk)
+
+sybau_paths=["picture/sybau1.png","picture/sybau2.png","picture/sybau3.png"]
+sybau=buttonObject(sybau_paths,(200,300),(200,200))
+main_menu_objectlist.add(sybau)
 
 title=pg.font.SysFont("arial",72)
 titletext=title.render("TEST MENU",True,(0,0,255))
+
+def main_menu():
+    screen.blit(mainMenuBg,(0,0))
+    screen.blit(titletext,(100,100))
+    main_menu_objectlist.update()
+    main_menu_objectlist.draw(screen)
 #main loop
 running=True
 while running:
     clock.tick(30)
     screen.blit(bg,(0,0))
-    screen.blit(mainMenuBg,(0,0))
-    screen.blit(titletext,(100,100))
     for event in pg.event.get():
         if event.type==pg.QUIT:
             running=False
-    objectlist.update()
-    objectlist.draw(screen)
+    main_menu()
     pg.display.update()
 pg.quit()
 
