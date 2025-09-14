2e49c68c28fb6c3d6392bb0415ce620c797acd04
Star1nDream <mc.lu.0806@gmail.com>
2025-09-14 09:27:42 +0800

    14-3

--- test_Star1nDream/test3.py
+++ test_Star1nDream/test3.py
@@ -5,13 +5,14 @@ width,height= pg.display.Info().current_w, pg.display.Info().current_h
 screen=pg.display.set_mode((width,height))
 pg.display.set_caption("test3")
 
-bg=pg.Surface(screen.get_size())
-bg=bg.convert()
-bg.fill((93, 129, 199))
-screen.blit(bg,(0,0))
+#bg=pg.Surface(screen.get_size())
+#bg=bg.convert()
+#bg.fill((93, 129, 199))
+bgi=pg.image.load(r"C:\Users\yazhu\OneDrive\Desktop\Untitled_Project\fuckUpygame-1\test_Star1nDream\image\test3-1.png").convert()
+screen.blit(bgi,(0,0))  
 pg.display.update()
 
-bgi=pg.image.load("C:\Users\yazhu\OneDrive\Desktop\Untitled_Project\test_image\test3-1.png").convert()
+
 
 running= True
 while running:
