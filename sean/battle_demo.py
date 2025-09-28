0156b4a760c74fb68b6d3666ceadddbeac797a52
Charles-noob <charleshsu.11@gmail.com>
2025-09-28 15:42:48 +0800

    add a slider in battle_demo

--- sean/battle_demo.py
+++ sean/battle_demo.py
@@ -1,5 +1,12 @@
 import pygame
-import sys
+import sys,os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+base_dir = os.path.dirname(script_dir)
+#將父目錄路徑添加到 Python 的搜尋路徑中
+#sys.path.insert(0, ...) 將路徑添加到清單的最前面
+sys.path.insert(0, base_dir)
+import XddObjects as xo
 
 pygame.init()
 
@@ -15,7 +22,16 @@ BLACK = (0, 0, 0)
 x, y = WIDTH // 2, HEIGHT // 2
 size = 10
 speed = 5
-friction = 0.6
+default_friction = 0.6
+
+friction_rail_path=os.path.join(base_dir,"picture","sound_slider","slider_rail.png")
+friction_twist_path=os.path.join(base_dir,"picture","sound_slider","slider_twist.png")
+friction_rail=xo.sliderRailObject(friction_rail_path,(WIDTH//2,70),(300,10))
+friction_twist=xo.sliderTwistObject(friction_twist_path,
+                                    (WIDTH//2,70),(10,27),
+                                    0,0.99,default_friction,friction_rail)
+friction_font=pygame.font.SysFont("times new roman",20)
+friction_text=friction_font.render(f"friction: {friction_twist.current_val:.2f}",True,BLACK)
 
 vx, vy = 0, 0
 
@@ -24,16 +40,22 @@ clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT :
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key in [pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d]:
                 if event.key not in pressed_keys:
                     pressed_keys.append(event.key)
+            if event.key==pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
         elif event.type == pygame.KEYUP:
             if event.key in pressed_keys:
                 pressed_keys.remove(event.key)
+
+    friction_twist.update()
+    friction_rail.update()
 
     if pressed_keys:
         key = pressed_keys[-1] 
@@ -46,8 +68,8 @@ while True:
         elif key == pygame.K_d:
             vx, vy = speed, 0
     else:
-        vx *= friction
-        vy *= friction
+        vx *= friction_twist.current_val
+        vy *= friction_twist.current_val
 
     x += vx
     y += vy
@@ -72,6 +94,10 @@ while True:
     display_keys = [pygame.key.name(k) for k in pressed_keys]  
     keys_text = font.render(f"Pressed keys: {display_keys}", True, BLACK)
     screen.blit(keys_text, (10, 10))
-
+    screen.blit(friction_rail.image,friction_rail.rect)
+    screen.blit(friction_twist.image,friction_twist.rect)
+    friction_text=friction_font.render(f"friction: {friction_twist.current_val:.2f}",True,BLACK)
+    screen.blit(friction_text,(WIDTH//2-50,90))
+    
     pygame.display.flip()
     clock.tick(60)
