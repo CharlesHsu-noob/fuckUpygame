aad81d58eacb4cfa503e7b95117415c117b6c3dd
leochuchu <chuleo961030@gmail.com>
2025-10-01 00:28:03 +0800

    rrrrr

--- chuchu/practice1.py
+++ chuchu/practice1.py
@@ -1,39 +1,12 @@
+import os,sys
 import pygame as pg
-import sys
-class buttonObject(pg.sprite.Sprite):
-    def __init__(self,picture_paths,center,size):
-        super().__init__()
-        self._is_held=False# 內部狀態：追蹤滑鼠是否正按在按鈕上
-        self.ispress=False
-        self.images = [
-            pg.transform.scale(pg.image.load(picture_paths[0]).convert_alpha(), size),
-            pg.transform.scale(pg.image.load(picture_paths[1]).convert_alpha(), size),
-            pg.transform.scale(pg.image.load(picture_paths[2]).convert_alpha(), size)
-        ]
-        self.image=self.images[0]
-        self.rect=self.image.get_rect(center=center)
-    def update(self):
-        self.ispress = False
-        mouse_pos = pg.mouse.get_pos()
-        mouse_down = pg.mouse.get_pressed()[0]
-        is_mouse_over = self.rect.collidepoint(mouse_pos)
-
-        if is_mouse_over:
-            if mouse_down:
-                # 情況1: 滑鼠在按鈕上，且正被按住
-                self.image = self.images[2] 
-                self._is_held = True
-            else:
-                # 情況2: 滑鼠在按鈕上，但沒有被按住
-                self.image = self.images[1] 
-                # 如果上一幀是按住的狀態，代表滑鼠剛被釋放，這就是一次 "點擊"
-                if self._is_held:
-                    self.ispress = True
-                self._is_held = False
-        else:
-            # 情況3: 滑鼠不在按鈕上
-            self.image = self.images[0]
-            self._is_held = False
+script_dir = os.path.dirname(os.path.abspath(__file__))
+base_dir = os.path.dirname(script_dir)
+# 將父目錄路徑添加到 Python 的搜尋路徑中
+#    sys.path.insert(0, ...) 將路徑添加到清單的最前面
+sys.path.insert(0, base_dir)
+import XddObjects as xo
+from XddObjects import buttonObject
 
 pg.init()
 WIDTH, HEIGHT = 800, 600
@@ -42,7 +15,7 @@ clock = pg.time.Clock()
 
 # 建立「暫停」按鈕
 pause_btn = buttonObject(
-    ["pause_normal.png", "pause_hover.png", "pause_pressed.png"], 
+    ["exit1", "exit2", "exit3"], 
     center=(60, 30), size=(100, 50)
 )
 buttons = pg.sprite.Group(pause_btn)
@@ -62,7 +35,8 @@ while True:
     if pause_btn.ispress:
         paused = not paused
 
-
+    # --- 畫面 ---
+    screen.fill((50, 120, 180))
 
     if paused:
         # 半透明黑幕
